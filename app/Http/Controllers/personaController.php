9ce958e1e519cc97bb010016d62b2d708fc798ca
Thiago-ch07 <thchidic@gmail.com>
2024-07-26 12:36:59 -0300

    Baja persona

--- app/Http/Controllers/personaController.php
+++ app/Http/Controllers/personaController.php
@@ -7,7 +7,7 @@ use App\Models\Personas;
 
 class personaController extends Controller
 {
-    public function store ( Request $request)
+    public function store (Request $request)
     {
         $persona = personas::create([
             'nombre' => $request->nombre,
@@ -15,5 +15,12 @@ class personaController extends Controller
             'telefono' => $request->telefono,
         ]);
         return response()->json($persona);
+    }
+
+    public function delete (Request $request)
+    {
+        $persona = personas::find($request->id);
+        $persona->delete();
+        return response()->json(['message' => 'Persona eliminada correctamente']);
     }
 }
